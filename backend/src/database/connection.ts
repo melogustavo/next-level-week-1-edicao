70221c33d4caf8edd9f337b0333f63e5a3461611
melo_gustavo <gustavomelo95@gmail.com>
2020-06-02 11:30:39 -0300

    displaying images with express static

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -3,10 +3,11 @@ import path from 'path';
 
 const connection = knex({
   client: 'sqlite3',
+  // Necessario apenas quando ta usando o sqlite
+  useNullAsDefault: true,
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite'),
   },
-  useNullAsDefault: true,
 });
 
 export default connection;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  '/uploads',
+  express.static(path.resolve(__dirname, 'assets', 'uploads')),
+);
 
 app.listen(3333, () => {
   console.log('Server listening on port 3333');
